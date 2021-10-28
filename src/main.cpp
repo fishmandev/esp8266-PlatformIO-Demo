ab77ea69c2196a04631d00cca39cc8e2b97acc00
Dmitriy Fishman <fishman.code@gmail.com>
2021-10-28 11:06:28 +0300

    Added a LED toggle

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
+#define ledPin 16
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, LOW);
+  Serial.println("LED is ON!");
+  delay(1000);
+
+  digitalWrite(ledPin, HIGH);
+  Serial.println("LED is OFF!");
+  delay(1000);
 }
